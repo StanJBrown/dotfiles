e02bec4953a61c1e46623ed63c7ada75d42b76b1
Stanley Brown <stan.j.brown@gmail.com>
2017-11-23 10:39:54 -0500

    Add install rules and a new alais....

---
+++ scripts/install/install_teensy_udev.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+setup_udev() {
+  sudo cp udev/teensy.rules /etc/udev/rules.d/49-teensy.rules
+  #Add user to dialout if they have not been
+  sudo adduser $USER dialout
+ 
+  #get rid of modemmanger, its always in the way
+  sudo apt-get remove --purge modemmanger
+
+  #reload udev
+  udevnadm control --reload-rules && udevadm trigger
+}
+
+setup_udev
